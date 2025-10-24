091a60cf2a39febfec919a61d804f339134ce2ec
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-10-24 06:14:54 -0500

    feat(command-log): read host names from configuration
    pull the host names out of configuration so the values can be configured
    via the config command

--- @mzm/command-log/search/command.ts
+++ @mzm/command-log/search/command.ts
@@ -1,7 +1,10 @@
 import {MZMCommand, EnumType} from '@mzm/core'
 import {toArray} from '@mzm/core/lang'
 import {pprint} from '@mzm/log';
+import {storage} from '@mzm/config'
+import {debuglog} from 'node:util'
 
+const log = debuglog('cmd:log')
 
 //TODO(esatterwhite) add a temp storage interface
 async function lastID() {
@@ -20,11 +23,13 @@ const search = new MZMCommand()
   .option('-l, --level <level:string>', 'log levels to filter by', {collect: true})
   .option('-a, --app <app:string>', 'log levels to filter by', {collect: true})
   .option('-n, --limit <lines:number>', 'Maximum Number of lines to request', {default: 10})
+  .group('Pagination options')
   .option('-p, --prefer <prefer:preference>', 'Get lines from the beginning of the interval rather than the end', {default: 'tail'})
   .option('--next', 'Get next chunk of lines (after last search). This is a convenience wrapper around the --from and --to parameters.')
   .option('--timeframe <timeframe>', 'Natural Language Timeframe via Chrono. Wrap in quotes. IE "today 5PM to 7PM" or "yesterday at 3PM to now" or "May 26 at 4PM UTC". If only one time is given, "from" is assumed.')
   .option('--from <from:number>', 'Unix/Natural Language timestamp of beginning of search timeframe. Wrap in quotes if NL. Ignored if --timeframe used.')
   .option('--to <to:number>', 'Unix/Natural Language timestamp of end of search timeframe. Wrap in quotes if NL. Ignored if --timeframe used.')
+  .group('Output Options')
   .option('-j, --json', 'Output raw JSON')
   .action(async (options: any, query?: string) => {
     const params: Record<string, string> = {
@@ -36,14 +41,17 @@ const search = new MZMCommand()
     if (options.limit) params.size = options.limit
     if (options.to) params.to = new Date(parseInt(options.to)).getTime().toString()
     if (options.host) params.hosts = toArray(options.host).join(',')
-    if (params.tag) params.tag = toArray(options.tag).join(',')
-    if (params.level) params.level = toArray(options.level).join(',')
-    if (params.app) params.app = toArray(options.app).join(',')
-
+    if (options.tag) params.tags = toArray(options.tag).join(',')
+    if (options.level) params.levels = toArray(options.level).join(',')
+    if (options.app) params.apps = toArray(options.app).join(',')
 
     if (options.next) params.pagination_id = await lastID() ?? ''
 
+    const API_HOST = await storage.getOne('core.host.api')
     const controller = new AbortController()
+
+    log('api host: %s', API_HOST)
+
     Deno.addSignalListener('SIGTERM', () => {
       controller.abort()
     })
