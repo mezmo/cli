7710d64bf1d1675962182e0e6d55f355f2a2974e
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-10-27 21:50:24 -0500

    feat(command-get): start of basic get command
    command to get one or more resources in various formats, table, json or
    yaml. includes the view resource sub command

---
+++ @mzm/command-get/view/command.ts
@@ -0,0 +1,78 @@
+import {MZMCommand, EnumType, Table, Cell, RowType, toTitleCase, yaml} from '@mzm/core'
+import {default as resource} from '@mzm/core/resource'
+import type {View} from '@mzm/core/resource'
+type Namespace = Record<string, RowType[]>
+
+const OutputFormat = new EnumType(['table', 'json', 'yaml'])
+
+export default new MZMCommand()
+  .name('view')
+  .type('format', OutputFormat)
+  .description('Interact with views - Predefined sets of log queries')
+  .arguments('[view-id:string]')
+  .option('-c, --category <category:string>', 'Specific viescategories to include', {collect: true})
+  .option('-q, --quiet', 'output only the resource identifiers', {default: false})
+  .option('-o, --output [format:format]', 'output only the resource identifiers', {default: 'table'})
+  .action(async function(options: any, view_id?: string) {
+    const output = new Table()
+
+    const body: RowType[] = []
+    if (view_id) {
+      output.header(['Category', 'Name', 'Apps', 'Hosts', 'Query'])
+      const response = await resource.view.get(view_id)
+      const view: View = response.data as View
+
+      if (options.quiet) return console.log(view.viewid)
+      switch(options.output) {
+        case 'json': {
+          return console.log(JSON.stringify(view, null, 2))
+        }
+        case 'yaml': {
+          return console.log(yaml.stringify(view))
+        }
+        case 'table': {
+          body.push([
+            toTitleCase((view.category[0] ?? 'Primary'))
+          , view.name
+          , view.apps?.join?.(', ')
+          , view.hosts?.join?.(', ')
+          , view.query
+          ])
+          return output.body(body).padding(8).render()
+        }
+      }
+    }
+
+    const {data: views} = await resource.view.list()
+    switch(options.output) {
+      case 'json': {
+        return console.log(JSON.stringify(views, null, 2))
+      }
+      case 'yaml': {
+        return console.log(yaml.stringify(views))
+      }
+      case 'table': {
+        output.header(['Category', 'Name', 'ID'])
+        if (options.quiet) return console.log(views.map((view: View) => {return view.viewid}).join(' '))
+        const categories: Namespace = {}
+        for (const view of views) {
+          const category = toTitleCase(view.category[0] ?? 'Primary')
+          const group = (categories[category] = categories[category] ?? [])
+
+          group.push(
+            [view.name, view.viewid]
+          )
+        }
+
+        for (const [category, rows] of Object.entries(categories)) {
+          const first_row = rows.shift()
+          if (!first_row) continue
+          body.push([new Cell(category).rowSpan(rows.length + 1), ...first_row])
+          for (const row of rows) {
+            body.push(row)
+          }
+        }
+        output.body(body).padding(8).render()
+      }
+    }
+  })
