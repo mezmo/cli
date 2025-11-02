19b0d79e210342706e710da506c9be1577ca0a13
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-11-01 21:53:15 -0500

    feat(command-create): Initial pass at command to create resources
    Command that accepts a resource definition file and creates the resource
    appropriately

---
+++ @mzm/command-create/mod.ts
@@ -0,0 +1,43 @@
+import {EOL} from 'node:os'
+import * as path from '@std/path'
+import {MZMCommand, yaml, ValidationError} from '@mzm/core'
+import {getLogger} from '@mzm/log'
+import {default as resource} from '@mzm/core/resource'
+import {parse} from '@mzm/core/resource'
+
+const log = getLogger('default')
+export default new MZMCommand()
+  .name('create')
+  .description([
+    'Create multiple resources from a file or stdin.'
+  ].join(EOL))
+  .option('-f, --file <file:string>', 'Path to a resource definition file.')
+  .action(async function (options: any) {
+    if (typeof options.file !== 'string') {
+      throw new ValidationError('file option is required and must be a string;')
+    }
+    const file: string = options.file
+    const location: string = path.isAbsolute(file) ? options.file : path.join(Deno.cwd(), file)
+    const definition = await parse(location)
+
+    if (!definition) return
+
+    const [version, kind] = definition.type?.split('/')
+
+    if (!kind) {
+      throw new ValidationError(`Resource definiton missing type declaration.`)
+    }
+
+    if (!Object.hasOwnProperty.call(resource, version)) {
+      const types = Object.keys(resource).sort().join(', ')
+      throw new ValidationError(`Unknown resource type ${kind}. Must be one of: ${types}`)
+    }
+
+    try {
+      //@ts-ignore workaround for module indexing
+      const result = await (resource)[version][kind].create(definition.spec)
+      console.dir(result)
+    } catch (err) {
+      console.dir(err)
+    }
+  })
