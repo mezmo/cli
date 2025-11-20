e7db6db7b4b84c189b25424e6eec05f70af16bf0
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-11-20 10:12:51 -0600

    chore(ci): include semantic release setup for build step
    adds semantic release setup to generate binaries and a checksum file
    until proper code signing is implemented
    
    The assets will all be attached to the related github release.

---
+++ release.config.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const templates = require('./release/templates/index.js')
+const commit = require('./release/commit.js')
+const constants = require('./release/constants.js')
+const now = new Date()
+const year = now.getFullYear()
+const day = String(now.getDate()).padStart(2, '0')
+const month = String(now.getMonth() + 1).padStart(2, '0')
+
+module.exports = {
+  branches: ['main']
+, npmPublish: false
+, tarballDir: 'dist'
+, parserOpts: {
+    noteKeywords: ['BREAKING CHANGES', 'BREAKING CHANGE', 'BREAKING']
+  , headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/
+  , breakingHeaderPattern: constants.BREAKING_HEADER_REGEX
+  , headerCorrespondence: ['type', 'scope', 'subject']
+  , issuePrefixes: ['#', 'gh-']
+  , referenceActions: [
+      'close', 'closes', 'closed', 'fix'
+    , 'fixes', 'fixed', 'resolve', 'resolves'
+    , 'resolved', 'ref'
+    ]
+  }
+, writerOpts: {
+    transform: commit.transform
+  , commitPartial: templates.commit
+  }
+, releaseRules: [
+    {breaking: true, release: 'major'}
+  , {type: 'build', release: 'patch'}
+  , {type: 'ci', release: 'patch'}
+  , {type: 'chore', release: 'patch'}
+  , {type: 'doc', release: 'patch'}
+  , {type: 'feat', release: 'minor'}
+  , {type: 'fix', release: 'patch'}
+  , {type: 'lib', release: 'patch'}
+  , {type: 'perf', release: 'minor'}
+  , {type: 'refactor', release: 'patch'}
+  , {type: 'style', release: 'patch'}
+  , {type: 'test', release: 'patch'}
+  ]
+, plugins: [
+    ['@semantic-release/commit-analyzer', null]
+  , ['@semantic-release/release-notes-generator', null]
+  , ['@semantic-release/changelog', {
+      changelogTitle: '## Changelog'
+    }]
+  , ['@semantic-release/exec', {
+      "verifyRelease": "deno task compile:local"
+    , "prepare": "deno task compile"
+    }]
+  , ['@semantic-release/exec', {
+      "prepare": "release/shasum.sh"
+    }]
+  , ['@semantic-release/npm', null]
+  , ['@semantic-release/git', {
+      assets: ['package.json', 'CHANGELOG.md', '!**/node_modules/**']
+    , message: `release: ${year}-${month}-${day}, `
+        + 'Version <%= nextRelease.version %> [skip ci]'
+    }]
+  , ['@semantic-release/github', {
+      assets: [
+        'dist/*'
+      ]
+    }]
+  ]
+}
---
+++ release/constants.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  BREAKING_HEADER_REGEX: /^(\w*)(?:\((.*)\))?!: (.*)$/
+}
