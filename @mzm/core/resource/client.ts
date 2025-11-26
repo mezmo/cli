4b6d9c93ce5525dd8c5fa6634b2b7413e94d7f59
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-11-26 07:19:42 -0600

    fix(core): remove the extraneous aura request client
    This was originally put here to support the use of the Bearer
    authentication scheme, but now that aura is using our gateway auth, that
    is entirely frivolous as the auth scheme is translated before
    forwarding. Aura handles our internal trusted header scheme, so the
    standard Token auth scheme externally is sufficient.

--- @mzm/core/resource/client.ts
+++ @mzm/core/resource/client.ts
@@ -1,11 +1,8 @@
-import {debuglog} from 'node:util'
 import {createClient} from '@anitrend/request-client'
 import type {RequestConfig} from '@anitrend/request-client'
 import {storage} from '@mzm/config'
 import {AuthorizationError} from '../error.ts'
 import {colors} from '@cliffy/ansi/colors'
-
-const log = debuglog('api:request')
 
 const ACCESS_KEY: string | undefined = Deno.env.get('MZM_ACCESS_KEY') as string ?? ''
 
@@ -23,19 +20,13 @@ const client = createClient({
 , responseType: 'json'
 })
 
-const aura = createClient({
-  baseURL: await storage.getOne('core.host.api') as string
-, timeout: 1000 * 60 * 10
-, responseType: 'json'
-})
-
 // add auth key just before request as to not
 // print it when introspecting objects inline
 client.interceptors.request.use((config: ApiRequest): RequestConfig => {
   if (!ACCESS_KEY) throw AuthorizationError.from(
     `Make sure the environment variable ${colors.magenta('MZM_ACCESS_KEY')} is set`
   )
-  const {headers = {}, method, baseURL, url, params = {}} = config
+  const {headers = {}} = config
   config.headers = {
     'Content-Type': 'application/json'
   , 'Authorization': `Token ${ACCESS_KEY}`
@@ -44,18 +35,4 @@ client.interceptors.request.use((config: ApiRequest): RequestConfig => {
   return config as RequestConfig
 })
 
-aura.interceptors.request.use((config: ApiRequest): RequestConfig => {
-  if (!ACCESS_KEY) throw AuthorizationError.from(
-    `Make sure the environment variable ${colors.magenta('MZM_ACCESS_KEY')} is set`
-  )
-  const {headers = {}, method, baseURL, url, params = {}} = config
-  config.headers = {
-    'Content-Type': 'application/json'
-  , 'Authorization': `Bearer ${ACCESS_KEY}`
-  ,  ...headers
-  }
-  return config as RequestConfig
-})
-
 export default client
-export {aura}
