8b8cdbeb1f9f3e48670c79e251dc4eeb48164beb
Eric Satterwhite <esatterwhite@wi.rr.com>
2025-11-26 13:17:09 -0600

    fix(resource): moving some stray view types in to the right module
    There were some types left inline. This moves them to the types module.
    no functional changes

--- @mzm/command-delete/view/command.ts
+++ @mzm/command-delete/view/command.ts
@@ -5,8 +5,8 @@ export default new MZMCommand()
   .name('view')
   .description('Delete one or more views')
   .arguments('<identifiers...:string>')
-  .example('Delete a single view:', 'mzm delete conversation a3d194')
-  .example('Delete many view:', 'mzm delete conversation a3d194 fab3134')
+  .example('Delete a single view:', 'mzm delete view a3d194')
+  .example('Delete many view:', 'mzm delete view a3d194 fab3134')
   .action(async function (_: any, ...identifiers: Array<string>) {
     const removed: Array<string> = []
     for (const view_id of identifiers) {
