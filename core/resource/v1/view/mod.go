7c296ef7717a75c2dc19817a4744927e1af055b3
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-02-20 13:38:42 -0600

    chore(view): make it possible to delete a view by name
    This updates the view remove function to try to find a view by its name
    if the first delete returns a 404

--- core/resource/v1/view/mod.go
+++ core/resource/v1/view/mod.go
@@ -144,8 +144,11 @@ func (r *ViewResource) Remove(pk string) error {
 	}
 
 	switch res.StatusCode() {
-	case 200, 201, 204, 404:
+	case 200, 201, 204:
 		return nil
+	case 404:
+		// If initial delete returns 404, try to find view by name and delete it
+		return r.removeByName(pk)
 	case 400:
 		fmt.Printf("Bad request - API response:\n%s\n", res.String())
 		return errors.New("bad request: check your view specification")
@@ -164,6 +167,53 @@ func (r *ViewResource) Remove(pk string) error {
 
 func (r *ViewResource) RemoveBySpec(view *View) error {
 	return errors.New("RemoveBySpec() Not Implemented")
+}
+
+// removeByName attempts to find a view by name (case-insensitive) and delete it by ID
+func (r *ViewResource) removeByName(name string) error {
+	// Get all views
+	views, err := r.List(nil)
+	if err != nil {
+		return fmt.Errorf("failed to list views while searching by name: %w", err)
+	}
+
+	// Search for a view with matching name (case-insensitive)
+	for _, view := range views {
+		if strings.EqualFold(view.Name, name) {
+			// Found a match, delete using the view's ID
+			res, err := r.client.
+				R().
+				SetPathParam("pk", view.Viewid).
+				Delete("/v1/config/view/{pk}")
+
+			if err != nil {
+				return fmt.Errorf("failed to delete view by ID %s: %w", view.Viewid, err)
+			}
+
+			switch res.StatusCode() {
+			case 200, 201, 204:
+				return nil
+			case 404:
+				return fmt.Errorf("view with name '%s' (ID: %s) not found", name, view.Viewid)
+			case 400:
+				fmt.Printf("Bad request - API response:\n%s\n", res.String())
+				return errors.New("bad request: check your view specification")
+			case 401:
+				return errors.New(
+					"There was a problem authenticating the previous operation. Make sure your access key is still valid",
+				)
+			case 403:
+				return errors.New(
+					"Make sure you have the appropriate permissions to delete views in the appropriate account",
+				)
+			default:
+				return fmt.Errorf("unexpected error: status %d", res.StatusCode())
+			}
+		}
+	}
+
+	// No view found with the given name
+	return fmt.Errorf("view with name '%s' not found", name)
 }
 
 func (r *ViewResource) Update(template resource.IResourceTemplate[View]) (*View, error) {
