cdf89dcabce45f0730c5df7c0e07a866035d165d
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-02-26 11:00:34 -0600

    fix(log): fixes a bug in the log rendering module
    log levels are not always strings. sometimes they can be numbers. In
    this case, json marshaling breaks. This using a bit of type checking to
    figure out the type and adjusts the pretty printer to account for the
    numeric level values

--- commands/log/render.go
+++ commands/log/render.go
@@ -2,8 +2,10 @@ package log
 
 import (
 	JSON "encoding/json"
+	"fmt"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -27,6 +29,14 @@ const LVL_TRACE = "trace"
 const LVL_DEBUG = "debug"
 const SPACE = " "
 
+var center style.Alignment = style.NewAlignment(style.AlignCenter, style.AlignMiddle)
+var defaultStyle style.Style = style.New().
+	Foreground(style.RGB(108, 108, 108)).
+	Padding(style.NewPadding(0, 1, 0, 1)).
+	Width(10).
+	MaxWidth(10).
+	Align(center)
+
 type StyleValue struct {
 	Pattern *regexp.Regexp
 	Style   style.Style
@@ -40,7 +50,27 @@ type Message struct {
 	App       string `json:"_app"`
 	Msg       string `json:"message,omitempty"`
 	Line      string `json:"_line,omitempty"`
-	Level     string `json:"level"`
+	Level     any    `json:"level"`
+}
+
+// levelToString safely converts a level field of any type to a string
+func levelToString(level any) string {
+	switch v := level.(type) {
+	case string:
+		return v
+	case int:
+		return strconv.Itoa(v)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case float64:
+		// JSON numbers are unmarshaled as float64 by default
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case fmt.Stringer:
+		return v.String()
+	default:
+		// Fallback to sprintf for any other type
+		return fmt.Sprintf("%v", v)
+	}
 }
 
 func colorize(level string) string {
@@ -50,7 +80,7 @@ func colorize(level string) string {
 			return style.Render(def.Style, level)
 		}
 	}
-	return level
+	return style.Render(defaultStyle, level)
 }
 
 func text(message string, level string) string {
@@ -65,10 +95,10 @@ func text(message string, level string) string {
 // to optimize performance and mimimize memory usage
 func pprint(line Message, plain bool) string {
 	var message string
-	if line.Line != "" {
-		message = line.Line
-	} else {
+	if line.Msg != "" {
 		message = line.Msg
+	} else {
+		message = line.Line
 	}
 
 	if plain {
@@ -81,12 +111,13 @@ func pprint(line Message, plain bool) string {
 
 	}
 	t := time.UnixMilli(int64(line.Timestamp))
+	levelStr := levelToString(line.Level)
 	return strings.Join([]string{
 		style.Render(grayStyle, t.Format(TIME_FORMAT)),
 		style.Render(redStyle, line.Host),
 		style.Render(blueStyle, line.App),
-		colorize(strings.ToLower(line.Level)),
-		text(message, line.Level),
+		colorize(strings.ToLower(levelStr)),
+		text(message, levelStr),
 	}, SPACE)
 }
 
@@ -96,8 +127,6 @@ func init() {
 	redStyle = style.New().Foreground(colorRed)
 	blueStyle = style.New().Foreground(style.RGB(0, 175, 215))
 	grayStyle = style.New().Foreground(style.Color256(24))
-
-	center := style.NewAlignment(style.AlignCenter, style.AlignMiddle)
 
 	errorStyle := style.New().
 		Foreground(style.White).
@@ -115,13 +144,6 @@ func init() {
 		MaxWidth(10).
 		Align(center)
 
-	debugStyle := style.New().
-		Foreground(style.RGB(108, 108, 108)).
-		Padding(style.NewPadding(0, 1, 0, 1)).
-		Width(10).
-		MaxWidth(10).
-		Align(center)
-
 	traceStyle := style.New().
 		Foreground(style.RGB(0, 95, 215)).
 		Background(style.RGB(99, 99, 99)).
@@ -129,6 +151,8 @@ func init() {
 		Width(10).
 		MaxWidth(10).
 		Align(center)
+
+	var debugStyle style.Style = defaultStyle
 
 	warnStyle := style.New().
 		Foreground(style.Black).
